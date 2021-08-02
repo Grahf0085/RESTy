ceca3fe6b2b54b91e4e51477b3bc62763af717ff
Tucker Hoog <tuckerhoog@tutanota.com>
2021-08-02 13:45:31 -0400

    test pass

--- src/containers/Restless.test.jsx
+++ src/containers/Restless.test.jsx
@@ -1,26 +1,27 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Restless from './Restless';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Restless container', () => {
   it('container', async () => {
-    render(<Restless />);
+    render(<MemoryRouter><Restless /></MemoryRouter>);
 
-    // const { queryByPlaceholderText } = render(<Search />);
-    const input = screen.getByPlaceholderText('Url');
-
-    fireEvent.change(input, { target: { value: 
-    `https://api.themoviedb.org/3/movie/550?api_key=
-      7e8fcc3961c48e87b30d12499718d750`
+    const urlInput =  await screen.findByPlaceholderText('URL');
+    fireEvent.change(urlInput, { target: { value: 
+    'https://api.themoviedb.org/3/movie/550?api_key=7e8fcc3961c48e87b30d12499718d750'
     } });
-    expect(input.value).toBe(`https://api.themoviedb.org/3/movie/550?api_key=
-    7e8fcc3961c48e87b30d12499718d750`);
+    expect(urlInput.value).toBe('https://api.themoviedb.org/3/movie/550?api_key=7e8fcc3961c48e87b30d12499718d750');
 
-    // screen.getByText('Loading...');
-    const ul = await screen.findByTestId('list');
-    return waitFor(() => {
-      expect(ul).not.toBeEmptyDOMElement();
-    }, 5000);
+    const methodInput = await screen.findByLabelText('GET');
+    userEvent.click(methodInput);
+    fireEvent.change(methodInput, { target: { value: 'GET'
+    } });
+    expect(methodInput).toBeChecked('GET');
 
+    const section = await screen.findByTestId('side');
+    expect(section).toBeEmptyDOMElement();
   });
 });
